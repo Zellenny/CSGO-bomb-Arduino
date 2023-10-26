4e00e3fb5230b3ddb09c162f5fc2071d6253d7ce
Zellenny <daangamedev@gmail.com>
2023-10-26 08:59:59 +0200

    add pinmodes for led

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include "arduino.h"
 
 #define ledGreen 2
-#define ledRed 3
+#define ledRed 0
+#define ledBlue 4
 
 void setup() {
     pinMode(ledGreen, OUTPUT);
+    pinMode(ledRed, OUTPUT);
+    pinMode(ledBlue, OUTPUT);
 }
 
 void loop() {
     digitalWrite(ledGreen, HIGH);
 }
-//31642257878
-
